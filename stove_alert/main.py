9cf5aedb69d7c158b7ec41fdb8e8f10a08586431
Yang Yu <yang@opencare.com>
2014-11-22 22:44:19 -0500

    main file

---
+++ stove_alert/main.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+import cherrypy
+
+from controls.electronics import Electronics
+from controls.controller import Controller
+
+electronics = Electronics.instance()
+control = Controller.instance()
+control.initialize_components()
+
+class WebService(object):
+	exposed = True
+
+class ControllerService(WebService):
+	def GET(self):
+		return control.to_JSON()
+
+class RootService(WebService):
+	control = ControllerService()
+
+	def GET(self):
+		return "Hello!"
+
+if __name__ == "__main__":
+	conf = {
+		'/': {
+			'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+			'tools.sessions.on': False,
+			'tools.response_headers.on': True,
+			'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+		}
+	}
+	cherrypy.config.update({
+		'server.socket_host': '0.0.0.0',
+		'server.socket_port': 8090}
+		)
+	cherrypy.quickstart(RootService(), '/', conf)
